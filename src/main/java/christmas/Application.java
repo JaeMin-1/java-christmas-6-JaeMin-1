60129c769e39387bef549315555ab594ccdda4da
JaeMin-1 <39516729+JaeMin-1@users.noreply.github.com>
2023-11-12 04:00:45 +0900

    feat: 방문 날짜 입력 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,5 +1,6 @@
 package christmas;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +33,19 @@ public class Application {
         beverage.put("샴페인", 25000);
         allMenu.put("음료", beverage);
 
+        while (true) {
+            try {
+                System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+                String input = Console.readLine();
+                int day = Integer.parseInt(input);
+                if (day < 1 || day > 31) {
+                    throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+                }
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
 
     }
 }
