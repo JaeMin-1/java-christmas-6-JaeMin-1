2201dfa7a430a30e9a0ddbd99f32d99d9b3d080f
JaeMin-1 <39516729+JaeMin-1@users.noreply.github.com>
2023-11-12 09:19:27 +0900

    refactor: readOrder 세분화

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -20,9 +20,7 @@ public class InputView {
     public static Map<String, Integer> readOrder() {
         while (true) {
             try {
-                System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-                String input = Console.readLine();
-                String[] tempOrder = input.split(",");
+                String[] tempOrder = inputOrder();
                 return validateAllOrder(tempOrder);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -34,6 +32,13 @@ public class InputView {
         int day = InputValidation.validateDate(input);
         Date date = new Date(day);
         return date.getDate();
+    }
+
+    private static String[] inputOrder() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2, 레드와인-1, 초코케이크-1)");
+        String input = Console.readLine();
+        String[] order = input.split(",");
+        return order;
     }
 
     private static Map<String, Integer> validateAllOrder(String[] input) {
